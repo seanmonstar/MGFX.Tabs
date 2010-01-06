0ed107af6b8c4dbc9c59b946741b9544276be101
seanmonstar <sean@mcarthurgfx.com>
2010-01-06 11:04:18 -0700

    updated to newest source

--- Demos/tabs.js
+++ Demos/tabs.js
@@ -1,4 +1,17 @@
-//MGFX.Tabs. Copyright (c) 2008-2009 Sean McArthur <http://mcarthurgfx.com/>, MIT Style License.
+/*
+---
+script: tabs.js
+description: MGFX.Tabs, extension of base class that adds tabs to control the rotater. 
+authors: Sean McArthur (http://mcarthurgfx.com) 
+license: MIT-style license 
+requires:
+ core/1.2.4: '*'
+ more/1.2.4.1: [Fx.Elements]
+provides: [MGFX.Tabs]
+...
+*/
+
+//MGFX.Tabs. Copyright (c) 2008-2010 Sean McArthur <http://mcarthurgfx.com/>, MIT Style License.
 
 var MGFX = MGFX || {};
 
@@ -7,7 +20,11 @@ MGFX.Tabs = new Class({
 	Extends: MGFX.Rotater,
 	
 	options: {
-		autoplay: false
+		autoplay: false,
+		onShowSlide: function(slideIndex) {
+			this.tabs.removeClass('active');
+			this.tabs[slideIndex].addClass('active');
+		}
 	},
 	
 	initialize: function(tabs, slides, options){
@@ -22,9 +39,7 @@ MGFX.Tabs = new Class({
 				}
 			});
 		}
-		
-		this.parent(slides,options);
-		return this;
+		return this.parent(slides,options);
 	},
 	
 	createTabs: function () {
@@ -32,21 +47,9 @@ MGFX.Tabs = new Class({
 			tab.addEvent('click', function(event){ 
 				event.preventDefault();
 				this.showSlide(index);
-				this.stop();
+				this.stop(true);
 			}.bind(this));
 		}.bind(this));
-	}.protect(),
-	
-	activateTab: function(tabIndex) {
-		this.tabs.removeClass('active');
-		this.tabs[tabIndex].addClass('active');		
-	}.protect(),
-	
-	showSlide: function(slideIndex){
-		if(slideIndex == this.currentSlide) return this;
-		this.activateTab(slideIndex);
-		this.parent(slideIndex);
-		return this;
-	}
+	}.protect()
 	
 });
